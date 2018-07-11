5aa3f933c8e3137c190a96c61114f7e41e5432ca
Watson <wtsnjp@gmail.com>
2018-07-11 13:01:47 +0900

    update examples_spec.rb

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'llmk_helper'
+require 'fileutils'
 require 'pathname'
 
 RSpec.configure do |c|
@@ -10,35 +11,37 @@ RSpec.describe "Processing example", :type => :aruba do
   # constants
   PWD = Pathname.pwd
   EXAMPLE_DIR = PWD + "examples"
-  before(:all) { set_default_env }
+  WORKING_DIR = PWD + "tmp/aruba"
 
-  #context "llmk.toml" do
-  #  before(:each) { run_llmk }
-  #  before(:each) { stop_all_commands }
-  #  before(:each) { puts last_command_started.stderr }
-  #  it { expect(last_command_started).to be_successfully_executed }
-  #end
+  before(:all) { set_default_env }
+  before(:each) { FileUtils.cp_r "#{EXAMPLE_DIR}/.", WORKING_DIR }
+
+  context "llmk.toml" do
+    before(:each) { run_llmk }
+    before(:each) { stop_all_commands }
+    it { expect(last_command_started).to be_successfully_executed }
+  end
 
   context "default.tex" do
-    before(:each) { run_llmk "#{EXAMPLE_DIR}/default.tex" }
+    before(:each) { run_llmk "default.tex" }
     before(:each) { stop_all_commands }
     it { expect(last_command_started).to be_successfully_executed }
   end
   
   context "simple.tex" do
-    before(:each) { run_llmk "#{EXAMPLE_DIR}/simple.tex" }
+    before(:each) { run_llmk "simple.tex" }
     before(:each) { stop_all_commands }
     it { expect(last_command_started).to be_successfully_executed }
   end
   
   context "complex.tex" do
-    before(:each) { run_llmk "#{EXAMPLE_DIR}/complex.tex" }
+    before(:each) { run_llmk "complex.tex" }
     before(:each) { stop_all_commands }
     it { expect(last_command_started).to be_successfully_executed }
   end
   
   context "platex.tex" do
-    before(:each) { run_llmk "#{EXAMPLE_DIR}/platex.tex" }
+    before(:each) { run_llmk "platex.tex" }
     before(:each) { stop_all_commands }
     it { expect(last_command_started).to be_successfully_executed }
   end
